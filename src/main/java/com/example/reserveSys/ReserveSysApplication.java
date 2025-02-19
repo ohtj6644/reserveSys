c837f4d52ff22663761aaf56fff07ef0806456d6
ohtj6644 <ohtj6644@naver.com>
2025-02-19 23:56:43 +0900

    [20250219] 마리아db 연결

---
+++ src/main/java/com/example/reserveSys/ReserveSysApplication.java
@@ -0,0 +1,13 @@
+package com.example.reserveSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserveSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reserveSys/ReserveSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reserveSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
