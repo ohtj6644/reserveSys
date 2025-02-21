82b2f596add9e82755806261c1745274c261051d
ohtj6644 <ohtj6644@naver.com>
2025-02-22 00:47:38 +0900

    [20250222]도서목록 추가

--- src/main/java/com/example/reserveSys/Service/BookService.java
+++ src/main/java/com/example/reserveSys/Service/BookService.java
@@ -2,9 +2,14 @@ package com.example.reserveSys.Service;
 
 import com.example.reserveSys.Entity.Book;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -35,5 +40,12 @@ public class BookService {
             System.out.println("AI 데이터 생성오류");
         }
         bookRepository.save(book);
+    }
+
+    public Page<Book> getPage(int page){
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page,15,Sort.by(sorts));
+        return this.bookRepository.findAll(pageable);
     }
 }
--- src/main/java/com/example/reserveSys/view/ViewController.java
+++ src/main/java/com/example/reserveSys/view/ViewController.java
@@ -1,11 +1,19 @@
 package com.example.reserveSys.view;
 
 
+import com.example.reserveSys.Entity.Book;
+import com.example.reserveSys.Service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class ViewController {
+    private final BookService bookService;
 
     @GetMapping("/")
     public String library001(){
@@ -15,5 +23,14 @@ public class ViewController {
     @GetMapping("/library002")
     public String library002(){
         return "library_002";
+    }
+
+    @GetMapping("/library003")
+    public String library003(Model model,@RequestParam(value = "page",defaultValue = "0")int page){
+        Page<Book> bookPage = this.bookService.getPage(page);
+
+        model.addAttribute("paging",bookPage);
+
+        return "library_003";
     }
 }
